e3116b7b203f238d32c09f8d003cdf0aeccb1ee0
zelkovaria <hyein396@gmail.com>
2023-12-20 01:58:04 +0900

    2.4.8~9 태그 및 주석

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import "../src/App.css";
 
 function App() {
   const name = "쿼카";
-  return <div className="react">{name}</div>;
+  return (
+    <>
+      <div className="react">{name}</div>
+      //주석 /* 주석 */
+      {/* 주석 */}
+      <input />
+    </>
+  );
 }
 
 export default App;
