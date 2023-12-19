13dd517b2cebb7364be461d20102627ddb027e59
zelkovaria <hyein396@gmail.com>
2023-12-20 03:01:16 +0900

    3.3.3 defaultProps-기본값 설정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import "../src/App.css";
-import { Component } from "react";
+import MyComponent from "./MyComponent";
 
-class App extends Component {
-  render() {
-    const name = "react";
-    return <div className="react">{name}</div>;
-  }
-}
+const App = () => {
+  return <MyComponent name="React" />;
+};
 
 export default App;
