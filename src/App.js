3ef4ef3300dfd84f1dc1bc58c5a037cb8d6de392
zelkovaria <hyein396@gmail.com>
2023-12-20 01:41:59 +0900

    2.4.4 AND 연산자 사용한 조건부 렌더링

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import "../src/App.css";
 
 function App() {
   const name = "쿼카";
-  return (
-    <div>
-      {name === "쿼카" ? <h1>쿼카입니다.</h1> : <h2>쿼카가 아닙니다.</h2>}
-    </div>
-  );
+  return <div>{name === "쿼카" && <h1>쿼카입니다.</h1>}</div>;
 }
 
 export default App;
