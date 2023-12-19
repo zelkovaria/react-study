4ff7246d64f75227a49218540ba66c74241579f9
zelkovaria <hyein396@gmail.com>
2023-12-20 01:53:37 +0900

    2.4.7 class 대신 className

--- src/App.js
+++ src/App.js
@@ -2,19 +2,7 @@ import "../src/App.css";
 
 function App() {
   const name = "쿼카";
-  return (
-    <div
-      style={{
-        backgroundColor: "black",
-        color: "aqua",
-        fontSize: "48px",
-        fontWeight: "bold",
-        padding: 16,
-      }}
-    >
-      {name}
-    </div>
-  );
+  return <div className="react">{name}</div>;
 }
 
 export default App;
