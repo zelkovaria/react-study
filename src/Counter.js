62b9049c864316d5160c68183cd35ea9a4b77afe
zelkovaria <hyein396@gmail.com>
2023-12-25 03:52:23 +0900

    3.4.1.4 함수인자 전달, 콜백함수로 특정 작업 실행

--- src/Counter.js
+++ src/Counter.js
@@ -14,7 +14,15 @@ class Counter extends Component {
         <h1>바뀌지 않는 값 : {fixedNumber}</h1>
         <button
           onClick={() => {
-            this.setState({ number: number + 1 });
+            this.setState(
+              {
+                number: number + 1,
+              },
+              () => {
+                console.log("방금 setState가 호출되었습니다");
+                console.log(this.state);
+              }
+            );
           }}
         >
           +1
