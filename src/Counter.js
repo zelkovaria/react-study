e959c1b29fdadd7caff447869e9b8946599b0647
zelkovaria <hyein396@gmail.com>
2023-12-24 23:38:07 +0900

    3.4.1.1 state 객체 안에 여러 값이 있을 때

--- src/Counter.js
+++ src/Counter.js
@@ -5,13 +5,15 @@ class Counter extends Component {
     super(props);
     this.state = {
       number: 0,
+      fixedNumber: 0,
     };
   }
   render() {
-    const { number } = this.state;
+    const { number, fixedNumber } = this.state;
     return (
       <div>
         <h1>{number}</h1>
+        <h1>바뀌지 않는 값 : {fixedNumber}</h1>
         <button
           onClick={() => {
             this.setState({ number: number + 1 });
