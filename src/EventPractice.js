00c55cf66f08c2f70fea999cff8794fddef27105
zelkovaria <hyein396@gmail.com>
2023-12-26 01:38:33 +0900

    4.2.4 input 여러개 다루기

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -3,26 +3,22 @@ import { Component } from "react";
 class EventPractice extends Component {
   state = {
     message: "",
+    username: "",
   };
 
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleChange(e) {
+  handleChange = (e) => {
     this.setState({
-      message: e.target.value,
+      [e.target.name]: e.target.value,
     });
-  }
+  };
 
-  handleClick() {
-    alert(this.state.message);
+  handleClick = () => {
+    alert(this.state.username + ": " + this.state.message);
     this.setState({
       message: "",
+      username: "",
     });
-  }
+  };
 
   render() {
     return (
@@ -30,8 +26,15 @@ class EventPractice extends Component {
         <h1>이벤트 연습</h1>
         <input
           type="text"
+          name="username"
+          placeholder="사용자명"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
           name="message"
-          placeholder="아무거나 입력해보세요"
+          placeholder="아무거나 입력해 보세요"
           value={this.state.message}
           onChange={this.handleChange}
         />
