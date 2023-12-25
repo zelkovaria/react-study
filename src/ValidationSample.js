ec96555b22ebf39b185b14d54758351f8b5cbff8
zelkovaria <hyein396@gmail.com>
2023-12-26 02:30:01 +0900

    5.1 ref 사용해보기 기본 예시

---
+++ src/ValidationSample.js
@@ -0,0 +1,45 @@
+import { Component } from "react";
+import "./ValidationSample.css";
+
+class ValidationSample extends Component {
+  state = {
+    password: "",
+    clicked: false,
+    validated: false,
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      password: e.target.value,
+    });
+  };
+
+  handleButtonClick = () => {
+    this.setState({
+      clicked: true,
+      validated: this.state.password === "0000",
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          tpye="password"
+          value={this.state.password}
+          onChange={this.handleChange}
+          className={
+            this.state.clicked
+              ? this.state.validate
+                ? "success"
+                : "failure"
+              : ""
+          }
+        />
+        <button onClick={this.handleButtonClick}>검증하기</button>
+      </div>
+    );
+  }
+}
+
+export default ValidationSample;
