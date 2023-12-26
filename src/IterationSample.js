1c38bdf3050e779736d02f45a8329f9396b551f2
zelkovaria <hyein396@gmail.com>
2023-12-26 21:48:16 +0900

    6.4.3 데이터 제거 기능 구현하기

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -21,7 +21,16 @@ const IterationSample = () => {
     setInputText("");
   };
 
-  const namesList = names.map((name) => <li key={name.id}>{name.text}</li>);
+  const onRemove = (id) => {
+    const nextNames = names.filter((name) => name.id !== id);
+    setNames(nextNames);
+  };
+
+  const namesList = names.map((name) => (
+    <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+      {name.text}
+    </li>
+  ));
   return (
     <>
       <input value={inputText} onChange={onChange} />
