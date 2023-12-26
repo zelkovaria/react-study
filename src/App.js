1734917f8fae0c9f70b0eb1cca32154910ccf1c0
zelkovaria <hyein396@gmail.com>
2023-12-26 22:59:30 +0900

    7.3 라이프사이클 메서드 사용하기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import "../src/App.css";
-import IterationSample from "./IterationSample";
 import { Component } from "react";
+import LifeCycleSample from "./LifeCycleSample";
+
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
   render() {
-    return <IterationSample />;
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color} />
+      </div>
+    );
   }
 }
 
