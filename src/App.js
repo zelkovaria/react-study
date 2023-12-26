006d094121c21a040550c3a25130e1cb47b5fd69
zelkovaria <hyein396@gmail.com>
2023-12-26 17:15:16 +0900

    6.2 데이터 배열을 컴포넌트 배열로 변환

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import "../src/App.css";
-import ScrollBox from "./ScrollBox";
+import IterationSample from "./IterationSample";
 import { Component } from "react";
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
-      </div>
-    );
+    return <IterationSample />;
   }
 }
 
