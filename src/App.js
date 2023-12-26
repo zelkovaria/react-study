9d11fae1c52bcba5197747f50be007c6b575ce0e
zelkovaria <hyein396@gmail.com>
2023-12-26 17:00:00 +0900

    5.3.3~4 컴포넌트에 메서드 작성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import "../src/App.css";
-import ValidationSample from "./ValidationSample";
+import ScrollBox from "./ScrollBox";
+import { Component } from "react";
 
-const App = () => {
-  return <ValidationSample />;
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
